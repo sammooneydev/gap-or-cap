5bb2cd3c13befaff6c91df2b54eb08d7e44b9b27
Sam Mooney <sammooneydev@gmail.com>
2025-07-03 20:42:26 +0100

    added more quotes to the examplesentences array in define-gap.js

--- commands/define-gap.js
+++ commands/define-gap.js
@@ -7,6 +7,11 @@ module.exports = {
     const examplesentences = [
         "Yo dude did you see that set? That was such an insane gap I can't even lie.",
         "I felt so bad for Player 2 man, the gap was crazy",
+        "Player 2 whiffed dp. That was a gap",
+        "He got gapped despite playing a top-tier",
+        "He got gapped then blamed his controller",
+        "The gap was predetermined",
+        "That wasn't a mash, it was a gap",
     ]
     const example = examplesentences[Math.floor(Math.random() * examplesentences.length)];
     interaction.reply(
