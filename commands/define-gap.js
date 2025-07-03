445f40d83704b2492f374e548edfc657ad94ef7e
Sam Mooney <sammooneydev@gmail.com>
2025-07-03 20:17:48 +0100

    slight tweak to the formatting of the markdown within define-gap.js module (maybe more of these to come )

--- commands/define-gap.js
+++ commands/define-gap.js
@@ -10,7 +10,7 @@ module.exports = {
     ]
     const example = examplesentences[Math.floor(Math.random() * examplesentences.length)];
     interaction.reply(
-      "**The term 'gap'** — in the modern FGC, refers to a very large *skill gap* between two opponents. It describes when one player greatly outplays their opponent, leading to an easy victory.\n\n" +
+      "The term **'gap'** — in the modern FGC, refers to a very large *skill gap* between two opponents. It describes when one player greatly outplays their opponent, leading to an easy victory.\n\n" +
         "**Example:**\n" +
         `_"${example}"_`
     );
