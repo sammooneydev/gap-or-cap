eb4d6ed26ba722339338098ac7812491dccbb97f
Noysomee <124669661+Noysomee@users.noreply.github.com>
2025-07-05 01:38:16 +0100

    made formatting on submit-clip.js module code prettier

--- commands/submit-clip.js
+++ commands/submit-clip.js
@@ -1,22 +1,29 @@
 module.exports = {
-    data: {
-        name: 'submit-clip',
-        description: 'submit a clip',
-        options: [
-            {
-                name: 'clip',
-                description: 'attach your clip',
-                type: 11,
-                required: true,
-            },
-        ],
-    },
-    run: async ({ interaction }) => {
-        const clip = interaction.options.getAttachment('clip');
-        if (!clip || !clip.contentType.startsWith('video/')) {
-            return interaction.reply({ content: "Please attach a valid video file", ephemeral: true});
-        }
-        await interaction.reply({ content: 'Allllllrighty then chatroom, I have a simple question for you. GAP. OR. CAP.', files: [clip.url]});
-        //TODO: add process for actually polling the server, whether that be with reactions or with an actual poll
+  data: {
+    name: "submit-clip",
+    description: "submit a clip",
+    options: [
+      {
+        name: "clip",
+        description: "attach your clip",
+        type: 11,
+        required: true,
+      },
+    ],
+  },
+  run: async ({ interaction }) => {
+    const clip = interaction.options.getAttachment("clip");
+    if (!clip || !clip.contentType.startsWith("video/")) {
+      return interaction.reply({
+        content: "Please attach a valid video file",
+        ephemeral: true,
+      });
     }
-}
+    await interaction.reply({
+      content:
+        "Allllllrighty then chatroom, I have a simple question for you. GAP. OR. CAP.",
+      files: [clip.url],
+    });
+    //TODO: add process for actually polling the server, whether that be with reactions or with an actual poll
+  },
+};
