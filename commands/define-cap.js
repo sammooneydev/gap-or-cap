29e8b8188a67e9fd4ce65747a242105998a10467
Sam Mooney <sammooneydev@gmail.com>
2025-07-06 12:11:33 +0100

    fully commented define-cap.js module with the relevant information

--- commands/define-cap.js
+++ commands/define-cap.js
@@ -1,16 +1,20 @@
+//exports the 'define-cap' command for the bot
 module.exports = {
   data: {
     name: "define-cap",
     description: "defines the term 'cap' within this context",
   },
   run: ({ interaction }) => {
+    //array of example sentences showing how 'cap' would be used in a sentence
     const examplesentences = [
       "Calling THAT a gap is absolutely cap, he just messed up his dp",
       "Thats cap, you just lamed me out with a top tier",
       "You said you gapped me but thats cap bro, you were just mashing",
     ];
+    //randomly selecting a sentence from array
     const example =
       examplesentences[Math.floor(Math.random() * examplesentences.length)];
+    //sends my decided definition and the selected example to the user
     interaction.reply(
       "The term **'cap'** — in the current context, refers to a situation where the gap in question is in fact, **NOT** a gap.\n\n" +
         "**Example:**\n" +
