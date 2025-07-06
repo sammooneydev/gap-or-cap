040dada968f89ab4028cf2617df088627f507c7a
Sam Mooney <sammooneydev@gmail.com>
2025-07-06 11:37:54 +0100

    fully commented index.js module with the relevant information

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-require("dotenv/config");
-const { Client, IntentsBitField } = require("discord.js");
-const { CommandHandler } = require("djs-commander");
-const path = require("path");
+require("dotenv/config"); //dotenv file required to access the DISCORD_TOKEN environment variable
+const { Client, IntentsBitField } = require("discord.js"); 
+const { CommandHandler } = require("djs-commander"); 
+const path = require("path"); //defining node path module for use in working with directory paths
 
+// setting up the bot's persmissions within the codebase
+// This bot can access servers and their messages, as well as polls and reactions, and the content of messages
 const client = new Client({
   intents: [
     IntentsBitField.Flags.Guilds,
@@ -13,14 +15,16 @@ const client = new Client({
   ],
 });
 
+//setting up the command handler, which contains the route to the 'commands' directory
 new CommandHandler({
   client,
-  eventsPath: path.join(__dirname, "events"),
   commandsPath: path.join(__dirname, "commands"),
 });
 
+//small piece of code that logs the discord account information to the console once the bot is running
 client.once("ready", () => {
   console.log(`Logged in as ${client.user.tag}`);
 });
 
+//using the bot's discord token to launch the bot
 client.login(process.env.DISCORD_TOKEN);
