566c5ef2bb8e8ddfd2dafbb4667926675e7600d5
Sam Mooney <sammooneydev@gmail.com>
2025-07-06 11:49:37 +0100

    fully commented define-gap.js module with the relevant information

--- commands/define-gap.js
+++ commands/define-gap.js
@@ -1,9 +1,11 @@
+//exports the 'define-gap' command for the bot
 module.exports = {
   data: {
     name: "define-gap",
     description: "defines the term 'gap'",
   },
   run: ({ interaction }) => {
+    //array of example sentences showing how 'gap' would be used in a sentence
     const examplesentences = [
       "Yo dude did you see that set? That was such an insane gap I can't even lie.",
       "I felt so bad for Player 2 man, the gap was crazy",
@@ -13,8 +15,10 @@ module.exports = {
       "The gap was predetermined",
       "That wasn't a mash, it was a gap",
     ];
+    //randomly selecting a sentence from array
     const example =
       examplesentences[Math.floor(Math.random() * examplesentences.length)];
+    //sends my decided definition and the selected example to the user
     interaction.reply(
       "The term **'gap'** — in the modern FGC, refers to a very large *skill gap* between two opponents. It describes when one player greatly outplays their opponent, leading to an easy victory.\n\n" +
         "**Example:**\n" +
