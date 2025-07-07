0ceac0fa6fa2ed0002d09c6df597e4e22fd05afb
Sam Mooney <sammooneydev@gmail.com>
2025-07-07 13:44:40 +0100

    wrapped all of the code in the define-gap.js module within a try catch to handle any errors that may arise

--- commands/define-gap.js
+++ commands/define-gap.js
@@ -5,24 +5,28 @@ module.exports = {
     description: "defines the term 'gap'",
   },
   run: ({ interaction }) => {
-    //array of example sentences showing how 'gap' would be used in a sentence
-    const examplesentences = [
-      "Yo dude did you see that set? That was such an insane gap I can't even lie.",
-      "I felt so bad for Player 2 man, the gap was crazy",
-      "Player 2 whiffed dp. That was a gap",
-      "He got gapped despite playing a top-tier",
-      "He got gapped then blamed his controller",
-      "The gap was predetermined",
-      "That wasn't a mash, it was a gap",
-    ];
-    //randomly selecting a sentence from array
-    const example =
-      examplesentences[Math.floor(Math.random() * examplesentences.length)];
-    //sends my decided definition and the selected example to the user
-    interaction.reply(
-      "The term **'gap'** — in the modern FGC, refers to a very large *skill gap* between two opponents. It describes when one player greatly outplays their opponent, leading to an easy victory.\n\n" +
-        "**Example:**\n" +
-        `_"${example}"_`
-    );
+    try {
+      //array of example sentences showing how 'gap' would be used in a sentence
+      const examplesentences = [
+        "Yo dude did you see that set? That was such an insane gap I can't even lie.",
+        "I felt so bad for Player 2 man, the gap was crazy",
+        "Player 2 whiffed dp. That was a gap",
+        "He got gapped despite playing a top-tier",
+        "He got gapped then blamed his controller",
+        "The gap was predetermined",
+        "That wasn't a mash, it was a gap",
+      ];
+      //randomly selecting a sentence from array
+      const example =
+        examplesentences[Math.floor(Math.random() * examplesentences.length)];
+      //sends my decided definition and the selected example to the user
+      interaction.reply(
+        "The term **'gap'** — in the modern FGC, refers to a very large *skill gap* between two opponents. It describes when one player greatly outplays their opponent, leading to an easy victory.\n\n" +
+          "**Example:**\n" +
+          `_"${example}"_`
+      );
+    } catch (err) {
+      console.error("error in define-gap command: ", err);
+    }
   },
 };
