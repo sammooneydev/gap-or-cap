ab3be8443f6d5cf1e32f0f6ceac722594c1f54c6
Sam Mooney <sammooneydev@gmail.com>
2025-07-07 13:47:13 +0100

    wrapped all of the code within define-cap.js module in a try catch to handle any errors that may arise

--- commands/define-cap.js
+++ commands/define-cap.js
@@ -5,20 +5,24 @@ module.exports = {
     description: "defines the term 'cap' within this context",
   },
   run: ({ interaction }) => {
-    //array of example sentences showing how 'cap' would be used in a sentence
-    const examplesentences = [
-      "Calling THAT a gap is absolutely cap, he just messed up his dp",
-      "Thats cap, you just lamed me out with a top tier",
-      "You said you gapped me but thats cap bro, you were just mashing",
-    ];
-    //randomly selecting a sentence from array
-    const example =
-      examplesentences[Math.floor(Math.random() * examplesentences.length)];
-    //sends my decided definition and the selected example to the user
-    interaction.reply(
-      "The term **'cap'** — in the current context, refers to a situation where the gap in question is in fact, **NOT** a gap.\n\n" +
-        "**Example:**\n" +
-        `_"${example}"_`
-    );
+    try {
+      //array of example sentences showing how 'cap' would be used in a sentence
+      const examplesentences = [
+        "Calling THAT a gap is absolutely cap, he just messed up his dp",
+        "Thats cap, you just lamed me out with a top tier",
+        "You said you gapped me but thats cap bro, you were just mashing",
+      ];
+      //randomly selecting a sentence from array
+      const example =
+        examplesentences[Math.floor(Math.random() * examplesentences.length)];
+      //sends my decided definition and the selected example to the user
+      interaction.reply(
+        "The term **'cap'** — in the current context, refers to a situation where the gap in question is in fact, **NOT** a gap.\n\n" +
+          "**Example:**\n" +
+          `_"${example}"_`
+      );
+    } catch (err) {
+      console.error("error with define-cap command: ", err);
+    }
   },
 };
