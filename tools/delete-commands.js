e95f99868f790fafc8f9a958404c11362d7a292c
Sam Mooney <sammooneydev@gmail.com>
2025-07-13 12:50:21 +0100

    created new directory in project root called 'tools' which contains new delete-commands.js module, created to improve maintainability as it allows me to delete any commands that might be left over from testing

---
+++ tools/delete-commands.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const { REST } = require("discord.js");
+const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN);
+
+(async () => {
+  try {
+    const applicationId = process.env.APP_ID;
+    // Fetch all global commands
+    const commands = await rest.get(`/applications/${applicationId}/commands`);
+    for (const command of commands) {
+      await rest.delete(
+        `/applications/${applicationId}/commands/${command.id}`
+      );
+      console.log(`Deleted command ${command.name}`);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+})();
